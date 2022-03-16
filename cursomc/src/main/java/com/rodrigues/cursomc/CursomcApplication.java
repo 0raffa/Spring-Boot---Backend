bcdca72635a1362b8e3641fecd2ee83fcac74126
Rafael Rodrigues <raffa.dma@gmail.com>
2022-03-16 16:39:51 -0300

    primeiro commit

---
+++ cursomc/src/main/java/com/rodrigues/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigues.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
